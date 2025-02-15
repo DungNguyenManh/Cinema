5eb82bdc3cc0ccd97bb8586b6c435ca21c536bf8
DungNguyenManh <dung903698954@gmail.com>
2025-02-15 17:23:23 +0700

    Ciname

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -14,11 +14,48 @@ export const getUser = async () => {
 }
 
 export const createUser = async (req, res) => {
+
+    const { username, password, email, phone_number } = req.body
+
+    if (!username || !password || !email || !phone_number) {
+        return res.status(400).json({ success: false, message: 'Create user failed' })
+    }
+
     try {
         const user = await User.create(req.body)
         res.status(201).json(user)
     } catch (error) {
         console.error(error)
-        res.status(400).json({ success: false, message: 'Loi error' })
+        res.status(500).json({ success: false, message: 'Internal Server Error' })
+    }
+}
+
+export const updateUser = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: 'Invalid Product ID' })
+    }
+
+    try {
+        const updateUser = await User.findByIdAndUpdate(id, req.body, { new: true })
+        res.status(200).json({ success: true, data: updateUser })
+    } catch (error) {
+        res.status(404).json({ success: false, message: 'Server Error' })
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: 'Invalid Product ID' })
+    }
+
+    try {
+        await User.findByIdAndDelete(id)
+        res.status(200).json({ success: true, message: 'User deleted successfully' })
+    } catch (error) {
+        res.status(404).json({ success: false, message: 'Server Error' })
     }
 }
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { getUser, createUser } from '../controllers/user.controller.js'
+import { getUser, createUser, updateUser, deleteUser } from '../controllers/user.controller.js'
 
 const router = express.Router()
 
 router.get('/', getUser)
 router.post('/', createUser)
+router.put('/:id', updateUser)
+router.delete('/:id', deleteUser)
 
 export default router
