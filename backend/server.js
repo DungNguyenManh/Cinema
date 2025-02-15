b6c75723dd92ce27039a5d19264173f450ad7e85
DungNguyenManh <dung903698954@gmail.com>
2025-02-15 17:05:41 +0700

    cinema

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-import express from "express"
-import dotenv from "dotenv"
-import { connectDB } from "./src/config/db.js"
-import userRoutes from "./src/routes/user.route.js"
+import express from 'express'
+import dotenv from 'dotenv'
+import { connectDB } from './src/config/db.js'
+import userRoutes from './src/routes/user.route.js'
 
 dotenv.config()
 
@@ -9,9 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 3000
 app.use(express.json())
 
-app.use("/api/users", userRoutes)
+app.use('/api/users', userRoutes)
 
 app.listen(3000, () => {
     connectDB()
-    console.log("Server is running on http://localhost:" + PORT);
+    console.log('Server is running on http://localhost:' + PORT);
 });
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -23,4 +23,4 @@ const userSchema = new mongoose.Schema({
         collection: 'USER'
     }
 )
-export default mongoose.model("User", userSchema)
+export default mongoose.model('User', userSchema)
